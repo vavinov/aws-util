6d3951b6d4a504f0fe277d307b9ad8f1c43291c1
Sergey Vavinov <vavinov@yandex-team.ru>
2013-11-02 02:59:50 +0400

    working on resume support

--- src/main/java/ru/vavinov/aws/Main.java
+++ src/main/java/ru/vavinov/aws/Main.java
@@ -8,7 +8,6 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CompleteMultipartUploadRequest;
 import com.amazonaws.services.s3.model.CompleteMultipartUploadResult;
 import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
-import com.amazonaws.services.s3.model.InitiateMultipartUploadResult;
 import com.amazonaws.services.s3.model.PartETag;
 import com.amazonaws.services.s3.model.ProgressEvent;
 import com.amazonaws.services.s3.model.ProgressListener;
@@ -16,7 +15,7 @@ import com.amazonaws.services.s3.model.UploadPartRequest;
 import com.google.common.collect.Lists;
 
 public class Main {
-    private static final int PART_SIZE = 5 * 1048576;
+    private static final int PART_SIZE = 10 * 1048576;
 
     public static class PartRange {
         private final int number;
@@ -31,7 +30,7 @@ public class Main {
 
         @Override
         public String toString() {
-            return "#" + number + "@" + offset + "/" + length;
+            return "[#" + number + " offset=" + offset + " size=" + length + "]";
         }
     }
 
@@ -43,13 +42,12 @@ public class Main {
             result.add(new PartRange(number, offset, partLength));
             offset += partLength;
         }
-        System.err.println(result);
         return result;
     }
 
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.err.println("usage:  " + Main.class.getName() + " <bucket> <key> <file>");
+        if (args.length < 3) {
+            System.err.println("usage:  " + Main.class.getName() + " <bucket> <key> <file> [<uploadid>] [<start-part-n>]");
             System.exit(1);
         }
 
@@ -58,16 +56,27 @@ public class Main {
         final File file = new File(args[2]);
 
         final AmazonS3Client client = new AmazonS3Client(new SystemPropertiesCredentialsProvider());
-        final InitiateMultipartUploadResult upload = client.initiateMultipartUpload(
-                new InitiateMultipartUploadRequest(bucket, key));
-        System.out.println("Got uploadId=" + upload.getUploadId());
+
+        final String uploadId = (args.length > 3) ? args[3]
+                : client.initiateMultipartUpload(new InitiateMultipartUploadRequest(bucket, key)).getUploadId();
+
+        final int firstPartNumber = (args.length > 4) ? Integer.parseInt(args[4]) : 1;
+
+        System.out.println("Got uploadId=" + uploadId);
+
+        List<PartRange> partRanges = toRanges(file.length(), PART_SIZE);
+        System.out.println("Total parts=" + partRanges.size() + ", starting from " + firstPartNumber);
 
         List<PartETag> etags = Lists.newArrayList();
-        for (PartRange range : toRanges(file.length(), PART_SIZE)) {
+        for (PartRange range : partRanges.subList(firstPartNumber - 1, partRanges.size())) {
+            if (range.number < firstPartNumber) {
+                continue; // XXX
+            }
+
             System.out.print("Uploading part #" + range.number);
             System.out.flush();
             PartETag etag = client.uploadPart(new UploadPartRequest()
-                    .withUploadId(upload.getUploadId())
+                    .withUploadId(uploadId)
                     .withBucketName(bucket)
                     .withKey(key)
                     .withPartNumber(range.number)
@@ -94,7 +103,7 @@ public class Main {
         }
 
         CompleteMultipartUploadResult complete = client.completeMultipartUpload(
-                new CompleteMultipartUploadRequest(bucket, key, upload.getUploadId(), etags));
+                new CompleteMultipartUploadRequest(bucket, key, uploadId, etags));
 
         System.out.println(complete.getLocation());
     }
